3a68cd80fd80ce7e55ee4bdd2abc08c11c5b9ad8
vladlen-beilik <vladlen.beilik@gmail.com>
2025-05-04 10:16:24 +0200

    ref

--- src/AppServiceProvider.php
+++ src/AppServiceProvider.php
@@ -8,9 +8,10 @@ class AppServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        $this->app->singleton(LimiterService::class, function ($app) {
-            return new LimiterService($app->make(\Illuminate\Contracts\Cache\Repository::class));
-        });
+        $this->app->singleton(
+            LimiterService::class,
+            fn($app) => new LimiterService($app->make(\Illuminate\Contracts\Cache\Repository::class))
+        );
     }
 
     public function boot(): void
